b073681135d885b0bdf142b1a1c2d6720048fd18
Christian Bamatembera <chrisbama92@gmail.com>
2021-01-24 22:33:47 -0500

    made it headless

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*- 
 
 from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -9,7 +10,9 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 import random
 
-driver = webdriver.Firefox()
+options = Options()
+options.add_argument('-headless')
+driver = webdriver.Firefox(options=options)
 driver.get("https://www.tvaplus.ca/star-academie/vote")
 
 girls = ["sa-0001","sa-0002","sa-0003","sa-0004","sa-0004","sa-0004","sa-0005"] #Jeni is sa-0004
@@ -35,10 +38,16 @@ for inputBtn in inputList:
 
 
 driver.execute_script("arguments[0].scrollIntoView();", girlInput)
+ActionChains(driver).move_to_element(girlInput).perform()
 girlInput.click()
 
 driver.execute_script("arguments[0].scrollIntoView();", guyInput)
+ActionChains(driver).move_to_element(guyInput).perform()
 guyInput.click()
 
 driver.execute_script("arguments[0].scrollIntoView();", voteInput)
+ActionChains(driver).move_to_element(voteInput).perform()
 voteInput.click()
+
+print("Voting complete")
+
